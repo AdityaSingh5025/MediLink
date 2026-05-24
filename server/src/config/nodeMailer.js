dfee8455bbf6cecb1a63761cdb2cd3ee96252d8e
Aditya Singh <adityasngh5025@gmail.com>
2026-05-25 00:01:07 +0530

    using brevo

--- server/src/config/nodeMailer.js
+++ server/src/config/nodeMailer.js
@@ -4,17 +4,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false, // true only for port 465
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
-  
 });
 
-transporter.verify(function (error, success) {
+transporter.verify((error, success) => {
   if (error) {
-    console.error(" Email transporter error:", error);
+    console.error("Email transporter error:", error);
   } else {
     console.log("Email server ready");
   }
@@ -27,16 +28,24 @@ export const mailer = async (email, subject, html) => {
     }
 
     const info = await transporter.sendMail({
-      from: `"Aditya Singh" <${process.env.EMAIL_USER}>`,
+      from: `"MediLink" <${process.env.SMTP_USER}>`,
       to: email,
-      subject: subject,
+      subject,
       html,
     });
 
-    // console.log("Email sent:", info.messageId);
-    return { success: true, messageId: info.messageId };
+    console.log("Email sent:", info.messageId);
+
+    return {
+      success: true,
+      messageId: info.messageId,
+    };
   } catch (error) {
-    // console.error("Error sending email:", error);
-    return { success: false, error: error.message };
+    console.error("Error sending email:", error);
+
+    return {
+      success: false,
+      error: error.message,
+    };
   }
 };
